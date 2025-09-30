ce72f77f59143dd09fa5bd440ccb7382aef88d9d
VMila <vi-milanez@hotmail.com>
2025-09-30 20:26:48 -0300

    adicionando timer e profile

--- frontend/src/pages/Player.tsx
+++ frontend/src/pages/Player.tsx
@@ -1,4 +1,12 @@
 import VideoPlayer from '../components/VideoPlayer';
+import { useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const sampleApiData = [
   { id: { videoId: 'DeQ9CgfPgtI' }, snippet: { title: 'Planeta Terra' } },
@@ -8,11 +16,32 @@ const sampleApiData = [
 ];
 
 function App() {
+   const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <VideoPlayer videos={sampleApiData} />
-      </header>
+   <div className='flex flex-col w-screen h-screen bg-gray-100'>
+      <div className='w-full'>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Button color="inherit" onClick={() => navigate('/')}>Hackaton</Button>
+              </Typography>
+              <Button color="inherit" onClick={() => navigate('/signup')}>Cadastrar</Button>
+              <Button color="inherit" onClick={() => navigate('/login')}>Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </div>
+          <VideoPlayer videos={sampleApiData} />
     </div>
   );
 }
