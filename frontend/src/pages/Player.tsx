98b5690beec53f08e527768904744fdaff48366a
VMila <vi-milanez@hotmail.com>
2025-09-30 19:29:35 -0300

    player funcionando

--- frontend/src/pages/Player.tsx
+++ frontend/src/pages/Player.tsx
@@ -1,9 +1,9 @@
 import VideoPlayer from '../components/VideoPlayer';
 
 const sampleApiData = [
-  { id: { videoId: 'L8TB3b93y2E' }, snippet: { title: 'Planeta Terra' } },
-  { id: { videoId: 'Rk6n6aQn7DE' }, snippet: { title: 'Esqui na neve' } },
-  { id: { videoId: 'm2EnP2fEt4Y' }, snippet: { title: 'Areia Cinética' } },
+  { id: { videoId: 'DeQ9CgfPgtI' }, snippet: { title: 'Planeta Terra' } },
+  { id: { videoId: 'RUQ_j-nR1Kk' }, snippet: { title: 'Esqui na neve' } },
+  { id: { videoId: 'uo9irp7Q2yE' }, snippet: { title: 'Areia Cinética' } },
   { id: { videoId: 'It-d58Py22w' }, snippet: { title: 'Paisagem Noruega' } },
 ];
 
